84be74320ec446b60fe83e39654138456b573bb3
Harsh Mahadev Duche <79721045+ducheharsh@users.noreply.github.com>
2022-10-09 15:26:43 +0530

    I want to add selection_sort.py
    ## Program Explanation
    1. The user is prompted to enter a list of numbers.
    2. The list is passed to the selection_sort function.
    3. The sorted list is displayed.

---
+++ Python/selection_sort.py
@@ -0,0 +1,10 @@
+
+def isPalindrome(x):
+        x = str(x)
+        if len(x) <= 1:
+            return True
+        else:
+            return x[0] == x[-1] and isPalindrome(x[1:-1])
+
+x = int(input("Enter any integer: "))
+print(isPalindrome(x))            
